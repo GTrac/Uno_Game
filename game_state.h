1785a3facca4fb36afb0b1ba4b0c688b798221a1
GobbT <48895080+GobbT@users.noreply.github.com>
2021-04-29 20:26:14 -0400

    Update game_state.h

--- game_state.h
+++ game_state.h
@@ -16,20 +16,29 @@ class GameState{
     public:
     /// number of players in the game
     int numPlayers;
+    
     /// index of the current player
     int currentPlayerIndex;
+    
     /// number of cards the player can play this turn
     /// (usually 1 or 0. 0 in the cases when the last card was a skip or draw 2)
+    
     int numCardsToPlay;
+    
     /// number of cards to draw this turn (usually 0. 2 when last card was draw 2)
     int numCardsToDraw;
+    
     /// indicates whether the index of the next player is determined by incrementing
+    
     /// or decrementing. Toggled by the reverse card.
     TurnDirection turnDirection;
+    
     /// Constructor. Passed the number of players for the current game.
     GameState(int n);
+    
     /// Used to update the next turn.
     void nextTurn();
+    
     /// Used to indicate if the player's turn should be skipped
     bool skipTurn = false;
 };
